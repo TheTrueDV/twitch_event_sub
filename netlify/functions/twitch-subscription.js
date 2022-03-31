307bb522946efceedf9ada224090677bd399a3c1
TheTrueDV <matthewjdavison1+dv@outlook.com>
2022-04-01 00:18:32 +0100

    update

--- netlify/functions/twitch-subscription.js
+++ netlify/functions/twitch-subscription.js
@@ -1,7 +1,7 @@
 export async function handler(event) {
 	const {headers = [] } = event;
 
-	const type = headers['twitch-eventsub-message-type'] || 'no type';
+	const type = headers['twitch-eventsub-message-type'] ?? 'no type';
 
 	return {
 		statusCode: 200,
